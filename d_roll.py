9962726194ea048a02b6c6192af0ca5a7423c836
Arihant Kuba <xerorex@Arihants-MacBook-Pro.local>
2021-06-17 04:16:03 +0530

    adding d_roll.py

---
+++ d_roll.py
@@ -0,0 +1,10 @@
+#Arihant Kunda
+#June 16, 2021
+#Idea for project taken from: https://www.youtube.com/watch?v=6tNS--WetLI
+
+import random
+
+def roll_dice():
+    """sumlates a dice roll"""
+    return random.randint(1, 6)
+
